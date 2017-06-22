7ecd2a3dde911acd8949cbfc20a5dcdb8585bdbf
DoubleTap347 <seandanzo@live.com>
2017-06-22 18:33:48 +0800

    Second Commit
    Need to add class data, need to program buttons

--- OS - Assessment 3/OS - Assessment 3/Classes/Vehicle.cs
+++ OS - Assessment 3/OS - Assessment 3/Classes/Vehicle.cs
@@ -9,14 +9,28 @@ namespace OS___Assessment_3.Classes
     class Vehicle
     {
         //for consistency I make the start of every word in the variables begin with capitals. First, First_Name.
-        string Manufacturer;
-        string Model;
-        string Make_Year;
-        string Registration;
-        double Kilometers_Travelled;
-        int Total_Services;
-        double Fuel_Economy;
-        bool Requires_Service;
-        double Fuel_Used;
+        string Manufacturer { get; set; }
+        string Model { get; set; }
+        string Make_Year { get; set; }
+        string Registration { get; set; }
+        double Kilometers_Travelled { get; set; }
+        int Total_Services { get; set; }
+        double Fuel_Economy { get; set; }
+        bool Requires_Service { get; set; }
+        double Fuel_Used { get; set; }
+        //Upper case beginnings then lower case beginnings
+        public Vehicle(string manufacturer, string model, string make_year, string registration, double kilometers_travelled, int total_services, double fuel_economy, bool requires_service, double fuel_used)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Make_Year = make_year;
+            Registration = registration;
+            Kilometers_Travelled = kilometers_travelled;
+            Total_Services = total_services;
+            Fuel_Economy = fuel_economy;
+            Requires_Service = requires_service;
+            Fuel_Used = fuel_used;
+        }
     }
+
 }
--- OS - Assessment 3/OS - Assessment 3/Form1.Designer.cs
+++ OS - Assessment 3/OS - Assessment 3/Form1.Designer.cs
@@ -28,12 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnNewJourney = new System.Windows.Forms.Button();
+            this.btnService = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.listBoxVehicleData = new System.Windows.Forms.ListBox();
+            this.txtTravelled = new System.Windows.Forms.TextBox();
+            this.txtFuelUsed = new System.Windows.Forms.TextBox();
+            this.txtRentTime = new System.Windows.Forms.TextBox();
+            this.txtReg = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.cmbVehicle = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vehicle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Make";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Vehicle Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(290, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Reg Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Model";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Journey";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(149, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Fuel used";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(290, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Rent time (days)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 223);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Travelled";
+            // 
+            // btnNewJourney
+            // 
+            this.btnNewJourney.Location = new System.Drawing.Point(17, 321);
+            this.btnNewJourney.Name = "btnNewJourney";
+            this.btnNewJourney.Size = new System.Drawing.Size(104, 23);
+            this.btnNewJourney.TabIndex = 9;
+            this.btnNewJourney.Text = "New Journey";
+            this.btnNewJourney.UseVisualStyleBackColor = true;
+            // 
+            // btnService
+            // 
+            this.btnService.Location = new System.Drawing.Point(127, 321);
+            this.btnService.Name = "btnService";
+            this.btnService.Size = new System.Drawing.Size(125, 23);
+            this.btnService.TabIndex = 10;
+            this.btnService.Text = "Completed a service";
+            this.btnService.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(686, 247);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear list";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(524, 114);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // listBoxVehicleData
+            // 
+            this.listBoxVehicleData.FormattingEnabled = true;
+            this.listBoxVehicleData.Location = new System.Drawing.Point(380, 295);
+            this.listBoxVehicleData.Name = "listBoxVehicleData";
+            this.listBoxVehicleData.Size = new System.Drawing.Size(381, 212);
+            this.listBoxVehicleData.TabIndex = 15;
+            // 
+            // txtTravelled
+            // 
+            this.txtTravelled.Location = new System.Drawing.Point(17, 247);
+            this.txtTravelled.Name = "txtTravelled";
+            this.txtTravelled.Size = new System.Drawing.Size(100, 20);
+            this.txtTravelled.TabIndex = 16;
+            // 
+            // txtFuelUsed
+            // 
+            this.txtFuelUsed.Location = new System.Drawing.Point(152, 247);
+            this.txtFuelUsed.Name = "txtFuelUsed";
+            this.txtFuelUsed.Size = new System.Drawing.Size(100, 20);
+            this.txtFuelUsed.TabIndex = 17;
+            // 
+            // txtRentTime
+            // 
+            this.txtRentTime.Location = new System.Drawing.Point(293, 247);
+            this.txtRentTime.Name = "txtRentTime";
+            this.txtRentTime.Size = new System.Drawing.Size(100, 20);
+            this.txtRentTime.TabIndex = 18;
+            // 
+            // txtReg
+            // 
+            this.txtReg.Location = new System.Drawing.Point(292, 78);
+            this.txtReg.Name = "txtReg";
+            this.txtReg.Size = new System.Drawing.Size(100, 20);
+            this.txtReg.TabIndex = 21;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(151, 78);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(100, 20);
+            this.txtModel.TabIndex = 20;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(16, 78);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(100, 20);
+            this.txtMake.TabIndex = 19;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(423, 78);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 22;
+            // 
+            // cmbVehicle
+            // 
+            this.cmbVehicle.FormattingEnabled = true;
+            this.cmbVehicle.Location = new System.Drawing.Point(530, 78);
+            this.cmbVehicle.Name = "cmbVehicle";
+            this.cmbVehicle.Size = new System.Drawing.Size(231, 21);
+            this.cmbVehicle.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 547);
+            this.Controls.Add(this.cmbVehicle);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtReg);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.txtRentTime);
+            this.Controls.Add(this.txtFuelUsed);
+            this.Controls.Add(this.txtTravelled);
+            this.Controls.Add(this.listBoxVehicleData);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnService);
+            this.Controls.Add(this.btnNewJourney);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnNewJourney;
+        private System.Windows.Forms.Button btnService;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listBoxVehicleData;
+        private System.Windows.Forms.TextBox txtTravelled;
+        private System.Windows.Forms.TextBox txtFuelUsed;
+        private System.Windows.Forms.TextBox txtRentTime;
+        private System.Windows.Forms.TextBox txtReg;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.ComboBox cmbVehicle;
     }
 }
 
